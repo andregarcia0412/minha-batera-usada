34c1650968d5be87a07eab852611e1a4b8e0bcc9
andregarcia0412 <andregarcia0412@gmail.com>
2025-09-29 21:05:19 -0300

    Add: main page caroulsel

--- src/main/resources/static/minha-batera-usada/src/js/main.js
+++ src/main/resources/static/minha-batera-usada/src/js/main.js
@@ -1,1 +1,18 @@
 import "../css/main.css"
+
+const carouselImages = document.querySelector('.carousel-images')
+const images = document.querySelectorAll('.carousel-images img')
+const prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+
+let index = 0
+
+function showSlide(i){
+    index = (i+images.length) % images.length
+    carouselImages.style.transform = `TranslateX(${-index * 100}%)`
+}
+
+prevBtn.addEventListener('click', () => showSlide(index - 1))
+nextBtn.addEventListener('click', () => showSlide(index + 1))
+
+setInterval(() => showSlide(index+1),10000)
